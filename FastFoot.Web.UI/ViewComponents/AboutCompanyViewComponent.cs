f5eb1458b83cff65a3776f8ad3208cb816a68f34
jeyhunag <104305445+jeyhunag@users.noreply.github.com>
2023-08-05 13:32:55 +0400

    Create New View Component

---
+++ FastFoot.Web.UI/ViewComponents/AboutCompanyViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoot.Web.UI.ViewComponents
+{
+    public class AboutCompanyViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ FastFoot.Web.UI/ViewComponents/SpecialFoodViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoot.Web.UI.ViewComponents
+{
+    public class SpecialFoodViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
