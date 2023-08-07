c896af152fc1235cf7ae830a5f00e7dbe4f60d33
jeyhunag <104305445+jeyhunag@users.noreply.github.com>
2023-08-07 17:31:22 +0400

    Create New Component2

---
+++ FastFood.DAL/Migrations/20230807121733_initQR.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastFood.DAL.Migrations
+{
+    public partial class initQR : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "QR",
+                table: "restaurants",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QR",
+                table: "orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "QR",
+                table: "foods",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QR",
+                table: "restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "QR",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "QR",
+                table: "foods");
+        }
+    }
+}
---
+++ FastFoot.Web.UI/ViewComponents/PopularMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FastFoot.Web.UI.ViewComponents
+{
+    public class PopularMenuViewComponent :ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
