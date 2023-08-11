2aa5c5b6682a90a5c6e1ed16faa9605bc8f97fdc
jeyhunag <104305445+jeyhunag@users.noreply.github.com>
2023-08-11 14:19:13 +0400

    Create Contact Message

---
+++ FastFoot.Web.UI/Areas/FoltAdmin/Controllers/ContactsController.cs
@@ -0,0 +1,49 @@
+﻿using FastFood.DAL.Data;
+using FastFood.DAL.DbModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastFoot.Web.UI.Areas.FoltAdmin.Controllers
+{
+    [Area("FoltAdmin")]
+    public class ContactsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ContactsController(AppDbContext db)
+        {
+            this._db = db;
+        }
+        // GET: CitiesController
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Contacts.ToListAsync());
+        }
+
+
+        // GET: CitiesController/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var contact = await _db.Contacts.FindAsync(id);
+            return View(contact);
+        }
+
+        // POST: CitiesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int Id, Contact contact)
+        {
+
+
+            await _db.Contacts.FindAsync(Id);
+            if (contact != null)
+            {
+                contact.DeletedDate = DateTime.Now;
+                _db.Contacts.Remove(contact);
+            }
+            _db.SaveChanges();
+            return RedirectToAction("index");
+
+        }
+    }
+}
--- FastFoot.Web.UI/Controllers/ContactController.cs
+++ FastFoot.Web.UI/Controllers/ContactController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FastFood.DAL.DbModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using FastFood.DAL.Data;
 
 namespace FastFoot.Web.UI.Controllers
 {
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext db;
+        public ContactController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Contact contact)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+                var configuration = builder.Build();
+                var host = configuration["Gmail:Host"];
+                var port = int.Parse(configuration["Gmail:Port"]);
+                var username = configuration["Gmail:Username"];
+                var password = configuration["Gmail:Password"];
+                var displayName = configuration["Gmail:DisplayName"];
+
+                var enable = bool.Parse(configuration["Gmail:SMTP:starttls:enable"]);
+
+                MailMessage mail = new MailMessage($"{contact.Email}", $"{username}");
+                mail.Subject = displayName;
+
+
+                mail.Body = $@"
+               <html>
+               <head> 
+               <style>
+              
+
+              </style>
+              </head>
+              <body>
+              <h2>Full Name: {contact.FullName} </h2>
+              <h2>City Name: {contact.CityName} </h2>
+              <h3>Email: {contact.Email}</h2> 
+               <h3>Message <br /> {contact.Message}</h3>
+            </body>
+            </html>";
+                mail.IsBodyHtml = true;
+                SmtpClient smtpClient = new SmtpClient(host, port);
+                smtpClient.Credentials = new NetworkCredential(username, password);
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(mail);
+
+
+                var category = await db.Contacts.AddAsync(contact);
+
+                if (category != null)
+                {
+                    return RedirectToAction("Create");
+                }
+
+                db.Contacts.AddAsync(contact);
+            }
+            return View(contact);
         }
     }
 }
